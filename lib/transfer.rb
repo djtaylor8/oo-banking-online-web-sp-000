3dce29fa0463748e2780c6632b59a8b73ba29042
DJ Taylor <dtaylor88@gmail.com>
2021-06-09 23:21:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?(sender, receiver)
+    if sender.valid? == true && receiver.valid? == true 
+      true 
+    else 
+      false 
+    end
+  end
+  
 end
