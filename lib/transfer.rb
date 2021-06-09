d9169dafdc3704b1a3d2efaa7feee5e28de1174d
DJ Taylor <dtaylor88@gmail.com>
2021-06-09 23:22:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
   
@@ -14,6 +16,7 @@ class Transfer
     else 
       false 
     end
+    binding.pry 
   end
   
 end
